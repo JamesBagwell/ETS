1a3937ea12bcaaeb3c1378c028ca6d6d1e275abd
James Bagwell <James_Bagwell@hotmail.co.uk>
2025-05-17 11:29:47 +0100

    Slight refactor on report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -6,12 +6,12 @@ from tkcalendar import Calendar
 import Writer
 import os
 
-# Function to get dates between two dates (including start and end dates)
+#GET DATES BETWEEEN TWO DATES INCLUDING START AND END DATES
 def get_dates_between(start_date, end_date):
     return [start_date + timedelta(days=i)
             for i in range((end_date - start_date).days + 1)]
 
-#Gets list of files between dates
+#GETS LIST OF FILES BETWEEN DATES
 def get_all_files(dates):
     files = []
     for dates in dates:
@@ -19,7 +19,7 @@ def get_all_files(dates):
             files.append(f"{base}/Reports/CSV Files/ETS {dates} WF")
     return files
 
-#Gets Dataframes for all files in file list and concats into 1 dataframe
+#GETS DATAFRAMES FOR ALL FILES IN FILE LIST AND CONCATS INTO SINGLE DATAFRAME
 def convert_to_df(list_of_files):
     start_date = start_datetime() #GET START DATE FOR MESSAGEBOX
     end_date = end_datetime() #GET END DAATE FOR MESSAGEBOX
@@ -28,30 +28,36 @@ def convert_to_df(list_of_files):
     files_not_found = 0
     for filename in list_of_files:
         try:
-            df = pd.read_csv(filename, index_col=None, header=0)
+            df = pd.read_csv(filename, index_col=None, header=0)  #ADDS ALL DATAFRAMES TO LIST OF DATAFRAMES
             df_list.append(df)
             files_found += 1
         except FileNotFoundError:
             files_not_found += 1
-    grouped_data = pd.concat(df_list, axis=0, ignore_index=True)
-    tk.messagebox.showinfo(title="File Summary",message=f"Report created for dates: {start_date} to {end_date}\nUsable files found: {files_found}\nFiles missing: {files_not_found}")
-    return  grouped_data
+    grouped_data = pd.concat(df_list, axis=0, ignore_index=True) #CONCATS ALL DATAFRAMES FROM LIST INTO ONE
+    tk.messagebox.showinfo(title="File Summary",
+                           message=f"Report created for dates: {start_date} to {end_date}\n"
+                                   f"Usable files found: {files_found}\n"
+                                   f"Files missing: {files_not_found}")
+    return  grouped_data #RETURNS SINGLE DATAFRAME
 
+#TAKES START DATE FROM WIDGET AND RETURNS IN CORRECT FORMAT
 def start_datetime():
-    start = datetime.strptime(start_date_calender.get_date(), '%m/%d/%y').strftime("%Y/%m/%d")
+    start = datetime.strptime(start_date_calendar.get_date(), '%m/%d/%y').strftime("%Y/%m/%d")
     start_tuple = tuple(start.split("/"))
     start_date = date(int(start_tuple[0]), int(start_tuple[1]), int(start_tuple[2]))
     return start_date
 
+#TAKES END DATE FROM WIDGET AND RETURNS IN CORRECT FORMAT
 def end_datetime():
-    end = datetime.strptime(end_date_calender.get_date(), '%m/%d/%y').strftime("%Y/%m/%d")
+    end = datetime.strptime(end_date_calendar.get_date(), '%m/%d/%y').strftime("%Y/%m/%d")
     end_tuple = tuple(end.split("/"))
     end_date = date(int(end_tuple[0]), int(end_tuple[1]), int(end_tuple[2]))
     return end_date
 
-#Get dates from widgets and create datafrane
+#GET DATES FROM WIDGETS AND CREATE DATAFRAME
 def get_dataframe_from_dates():
-    date_range = get_dates_between(start_date=start_datetime(), end_date=end_datetime()) #CREATED LIST OF DATES BETWEEN SELECTED DATES
+    date_range = get_dates_between(start_date=start_datetime(), #START DATE OF LIST OF DATES
+                                   end_date=end_datetime()) #END DATE OF LIST OF DATES
     files_for_dates = get_all_files(date_range) #GETS ALL FILES FOR DATES IN THE LIST
     main_df = convert_to_df(files_for_dates) # CONCATS ALL FILES COLLECTED INTO SINGLE DATAFRAME
     return main_df #RETURNS AS SINGLE DATAFRAME
@@ -62,18 +68,20 @@ def generate_multiple_report():
     end_date = end_datetime() #GET END DATE BASED ON USER INPUT FOR FILENAME CREATION
     filepath = f"{base}/Reports/Custom Reports/ETS {start_date} to {end_date}.xlsx" # USE TWO ABOVE VARIBLES
     main_csv = get_dataframe_from_dates() #CREATES USABLE VARIABLE FROM SELECTED DATAFRAMES
-    Writer.generate_multi_report(data=main_csv,filepath=filepath,template=template_path) #DATA = MAIN DATAFRAME / FILEPATH = EXCEL LOCATION / TEMPLATE = TEMPLATE LOCATION
+    Writer.generate_multi_report(data=main_csv, #DATA = MAIN DATAFRAME
+                                 filepath=filepath, #FILEPATH OF WHERE NEW EXCEL IS CREATED
+                                 template=template_path) #TEMPLATE FILEPATH
 
-#Today date
-current_datetime = datetime.now()
-day = current_datetime.day
-month = current_datetime.month
-year = current_datetime.year
+#TODAY'S DATE
+current_datetime = datetime.now() #RETURN FULL DATE
+day = current_datetime.day #TAKES DAY ONLY FROM FULL DATE
+month = current_datetime.month #TAKES MONTH ONLY FROM FULL DATE
+year = current_datetime.year #TAKES YEAR ONLY FROM FULL DATE
 
-#Base folder
-base = os.path.dirname(os.path.abspath(__file__))
+#BASE FOLDER
+base = os.path.dirname(os.path.abspath(__file__)) #USED TO MAKE MAINTAIN FILE STRUCTURE ON ALL COMPUTERS
 
-#GUI Set up
+#GUI SET UP
 root = tk.Tk()
 root.title("Engineering report generator")
 root.minsize(height=350,width=600)
@@ -81,27 +89,34 @@ root.maxsize(height=350,width=600)
 root.grid_columnconfigure(index=(0,1,2),weight=1)
 root.grid_rowconfigure(index=(0,1,2),weight=1)
 
-#Font Variables
+#FONT VARIABLES
 main_font = "Arial"
 label_font_size = 18
 main_pad = 5
 
-#Calender widgests
-start_date_calender = Calendar(root,selectmode="day",year=year,month=month,day=day)
-end_date_calender = Calendar(root,selectmode="day",year=year,month=month,day=day)
+#CALANDAR WIDGETS
+start_date_calendar = Calendar(root,selectmode="day", #CALENDAR FOR START DATE SELECTION
+                               year=year,
+                               month=month,
+                               day=day)
 
-#Labels
-date1_label = tk.Label(text="Start Date",font=(main_font,label_font_size),)
-date2_label = tk.Label(text="End Date",font=(main_font,label_font_size))
+end_date_calendar = Calendar(root,selectmode="day", #CALANDAR FOR END DATE SELECTION
+                             year=year,
+                             month=month,
+                             day=day)
 
-#Button
+#LABELS
+date1_label = tk.Label(text="Start Date",font=(main_font,label_font_size),) #START DATE LABEL
+date2_label = tk.Label(text="End Date",font=(main_font,label_font_size)) #END DATE LAVEL
+
+#BUTTONS
 button1 = tk.Button(text="Generate Report",command=generate_multiple_report,font=(main_font,18))
 button1.config(height=2,width=30,padx=main_pad,pady=main_pad,bg="Grey")
 
-#Grid Layout
-start_date_calender.grid(column=0,row=1)
+#GRID LAYOUT
+start_date_calendar.grid(column=0,row=1)
 date1_label.grid(column=0,row=2,sticky="new")
-end_date_calender.grid(column=2,row=1)
+end_date_calendar.grid(column=2,row=1)
 date2_label.grid(column=2,row=2,sticky="new")
 button1.grid(column=0,row=3,columnspan=3)
 
